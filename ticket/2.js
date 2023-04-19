bc13dbf96e11ddfd07327878df3bb5aca57393af
19Shakir <131231866+19Shakir@users.noreply.github.com>
2023-04-19 11:49:08 +0530

    Delete ticket directory

--- ticket/2.js
+++
@@ -1,9 +0,0 @@
-let userdetail= {
-    Name : "SHAKIR",
-    Contact : 8082191708,
-    From : "KASHMIR"
-    
-     
-
-}
-console.log(userdetail);
--- ticket/set.js
+++
@@ -1,4 +0,0 @@
- let courselist = ["dd", "hygy", "gg", "dd"];
- let subject = new Set(courselist);
- console.log(subject);
- console.log(subject.size);
